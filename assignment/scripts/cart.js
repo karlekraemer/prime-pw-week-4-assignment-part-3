9eb88d2f5d10f5bd46f5a620eb91012db8f5583f
Karl Kraemer <karlekraemer@gmail.com>
2022-10-25 17:30:50 -0500

    Try this instead

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,22 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+let basket = ['item', 'wallet', 'table'];
+function addItem( item) {
+    return true
+}
+console.log(addItem('item'));
+
+function listItems() {
+    for (let i = 0; i < basket.length; i++)
+    console.log(listItems('item'));
+}
+
+function empty() {
+    return basket.length = 0;
+}
+
+console.log(empty());
+console.log ( 'basket has', basket);
